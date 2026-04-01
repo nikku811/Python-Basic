1ef20b3c8f0e64ab31178413684ff6ccf6cc0211
Ram <nikkuvskm45@gmail.com>
2026-04-02 02:13:23 +0530

    Create docstring.py

---
+++ docstring.py
@@ -0,0 +1,16 @@
+
+
+
+def add(a, b):
+    """
+    This function adds two numbers.
+    
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+    
+    Returns:
+    int: The sum of the two numbers.
+    """
+    return a + b
+
