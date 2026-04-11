062bfbc91d1e8749352d7c91391cc5040162853e
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-11 20:57:50 +0530

    Create forloop.py

---
+++ Data Types/forloop.py
@@ -0,0 +1,6 @@
+#pseudo-code can
+
+#No_Of_Passengers=5
+#for(Passenger_Count=1,Passenger_Count<=No_Of_Passengers,Passenger_Count=Passenger_Count+1)
+#     display "Immigration check done for passenger,", Passenger_Count
+#end-for
