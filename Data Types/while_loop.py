f7e8f28c81c2f235da892b6123b72179376b7bab
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-11 20:57:40 +0530

    Create while_loop.py

---
+++ Data Types/while_loop.py
@@ -0,0 +1,7 @@
+no_of_passengers=3
+ticket_number=1001
+print("Ticket Numbers for all the Passengers:")
+while(no_of_passengers>0):
+  print("T -",ticket_number)
+  ticket_number=ticket_number+1
+  no_of_passengers=no_of_passengers-1
