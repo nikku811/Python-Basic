f37e020d451f6e1ed9bd7267b81579bcb53167cd
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-12 21:21:16 +0530

    Create Rules_for_var_3.py

---
+++ Python_Playlist/Chapter 2/Rules_for_var_3.py
@@ -0,0 +1,12 @@
+#RULES FOR CHOOSING AN IDENTIFIER
+'''A variable name can contain alphabets, digits, and underscores.
+A variable name can only start with an alphabet and underscores.
+A variable name can’t start with a digit.
+No while space is allowed to be used inside a variable name'''
+
+a = 56
+aaa = 5465
+carry = 45
+sameer = "hh"
+# %sammer = 54  # invalid name
+# @saneer = 45 # invalid name
