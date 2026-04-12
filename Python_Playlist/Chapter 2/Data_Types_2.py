dbf7f524460976948434f2225f1fa84b7ade8de9
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-12 21:21:05 +0530

    Create Data_Types_2.py

---
+++ Python_Playlist/Chapter 2/Data_Types_2.py
@@ -0,0 +1,7 @@
+# Data Types
+
+a = 2         # integer datatype
+b = 34.44     # floating datatype
+c = "Harry"   # String datatype
+d = True      # Boolean datatype
+e = None      # e is a none type variable that nothing hold any values
