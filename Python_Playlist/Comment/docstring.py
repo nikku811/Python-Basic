286c0486233ab07ba5d63f6cd3c671b35b0c2312
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-12 08:23:32 +0530

    Create docstring.py

---
+++ Python_Playlist/Comment/docstring.py
@@ -0,0 +1,16 @@
+'''winkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.'''
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.""")
