b2bf175dd2810a1cd536df5214ad13ae210d40ce
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-12 21:21:10 +0530

    Create operator_4.py

---
+++ Python_Playlist/Chapter 2/operator_4.py
@@ -0,0 +1,28 @@
+# 1. Arithmatic operater(=, -, *, /, //, % etc.)
+a = 223
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+# 1. Assignment operater (=, +=, -=, *=, /=, !=, etc.)
+
+a = 20-3 # assign 17 to a
+print(a)
+
+b = 15  # assign 15 to b
+b += 3 #Increment of 3 in var b
+print(b)
+b -= 6 #decrement of 3 in var b
+print(b)
+b = 15  # assign 15 to b
+b *= 5 #first b multiply by 3 after assign the var b
+print(b)
+b /= 2 # first b divide by 3 after assign var b
+print(b)
+b //= 3 
+print(b)
