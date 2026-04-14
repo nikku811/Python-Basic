6cd095274c67ffd1ab5f85ab1c2e434d8e1ba5ef
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-14 19:06:12 +0530

    Create inputFunc_6.py

---
+++ Python_Playlist/Chapter 2/inputFunc_6.py
@@ -0,0 +1,9 @@
+# input(): takes input as string
+a = input("Enter first no. : ")  # a=5
+b = input("Enter second no. : ") # b=6
+print(a + b) # output:56
+
+# to solve this issue we use 'int()'function for type convertion
+a = int(input("Enter first no. : "))  # a=5
+b = int(input("Enter second no. : ")) # b=6
+print(a + b) # output:11
