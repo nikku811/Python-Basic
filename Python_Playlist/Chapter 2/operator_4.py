aae9c804210252b4648201718afa074906f3933d
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-14 19:06:18 +0530

    Update operator_4.py

--- Python_Playlist/Chapter 2/operator_4.py
+++ Python_Playlist/Chapter 2/operator_4.py
@@ -9,7 +9,7 @@ print(a / b)
 print(a // b)
 print(a % b)
 
-# 1. Assignment operater (=, +=, -=, *=, /=, !=, etc.)
+# 2. Assignment operater (=, +=, -=, *=, /=, etc.)
 
 a = 20-3 # assign 17 to a
 print(a)
@@ -26,3 +26,35 @@ b /= 2 # first b divide by 3 after assign var b
 print(b)
 b //= 3 
 print(b)
+
+
+# 3. Comperision operater (==, >=, <=, >, <, != etc.)
+
+x = 13
+y = 2
+print(x == y)   #False
+print(y != x)   #True
+print(x < y)    #False
+print(x <= y)   #False
+print(x > y)    #True
+print(x >= y)   #True
+
+
+
+# 4. Logical Operators (and, or, not).
+
+print("Truth table if 'and' ")
+print(True and True)  #True
+print(True and False)  #False
+print(False and True)  #False
+print(False and False)  #False
+
+print("Truth table if 'or' ")
+print(True or True) #True
+print(True or False) #True
+print(False or True) #True
+print(False or False) #False
+
+print("Truth table if 'not' ")
+print(not True) #False
+print(not False) #True
