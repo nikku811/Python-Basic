99d55dc405d818eabd0dde0518bb85f0c2e55839
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-14 19:06:34 +0530

    Create Problem_3.py

---
+++ Python_Playlist/Chapter 2/Practice_Problem/Problem_3.py
@@ -0,0 +1,10 @@
+# 3. Check the type of variable assigned using input () function.
+a = input()
+b = input()
+c = input()
+d = input()
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
