184bf2e3b7eea04c3e855d3a9b93a9deb5fed66d
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-15 17:18:15 +0530

    Create Problem_4.py

---
+++ Python_Playlist/Chapter 2/Practice_Problem/Problem_4.py
@@ -0,0 +1,6 @@
+# 4. Use comparison operator to find out whether ‘a’ given variable a is greater than ‘b’ or not. Take a = 34 and b = 80.
+a = 34
+b = 80
+
+c = a > b
+print(c) # False : means a is smaller than b
