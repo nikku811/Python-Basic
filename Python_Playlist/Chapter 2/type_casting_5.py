17227eaf48102cb69c133ff7b7b13c04128974b1
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-15 17:18:31 +0530

    Create type_casting_5.py

---
+++ Python_Playlist/Chapter 2/type_casting_5.py
@@ -0,0 +1,16 @@
+# Type() and Typecasting
+
+# type(): used to find the type of data types which that variable hold
+
+# Emaple
+a = 35.7        # float value
+b = 34          # int value
+c = "345.66"    # string value
+d = type(a)     # type of "a" assign to "d"
+e = type(int(a)) # conerting into 'int'
+
+print(type(a))   # float
+print(type(b))   # int
+print(type(c))   # str
+print(d)   # float
+print(e)   # int
