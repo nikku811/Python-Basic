cda5970fb333ab56d4d53184df7662a02becdded
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-17 20:15:56 +0530

    Create sets.py

---
+++ Data Types/sets.py
@@ -0,0 +1,12 @@
+s1 = {10, 20, 30}
+print(s1)			# Output: {10, 20, 30}
+
+s2 = set([20, 30, 40])
+print(s2)			# Output: {40, 20, 30}
+
+s3 = {}
+print(type(s3))		# Output: <class 'dict'>
+
+s4 = set()
+print(type(s4))  	# Output: <class 'set'>
+print(s4)        	# Output: set()
