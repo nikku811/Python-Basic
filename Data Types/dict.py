a05d6c6c14983cedbcc3d3fd775786b6d9bc3814
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-18 23:20:20 +0530

    Create dict.py

---
+++ Data Types/dict.py
@@ -0,0 +1,8 @@
+d = {110:"xyz", 101:"abc", 105: "bcd", 104: "abc"}
+print(d)  			# Output: {110: 'xyz', 101: 'abc', 105: 'bcd', 104: 'abc'}
+d = {}
+d["laptop"] = 40000
+d["mobile"] = 15000
+d["earphone"] = 1000
+print(d)  			# Output: {'laptop': 40000, 'mobile': 15000, 'earphone': 1000}
+print(d["mobile"])  # Output: 15000
