6ae3babedfa10403d8b5c982ad682fca064455ff
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-22 15:05:46 +0530

    Create logical_operation.py

---
+++ Data Types/logical_operation.py
@@ -0,0 +1,4 @@
+a = False and True    							# Output: False
+b = True or False      							# Output: True
+c = False and print("This won't print")  		# Output: False
+d = True or print("This won't print either")  	# Output: True
