cd5e18352ee2bb179c2d3d5086b94278ca686626
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-25 03:14:19 +0530

    Create aithmatic_progression.py

---
+++ Data Types/aithmatic_progression.py
@@ -0,0 +1,10 @@
+# Program to calculate nth term of an arithmetic progression
+a = 5000  # First term
+d = 2000  # Common difference
+n = 61    # Term number
+
+# Calculate the nth term
+nth_term = a + (n - 1) * d
+
+# Output the result
+print("The 61st term of the arithmetic progression is:", nth_term)
