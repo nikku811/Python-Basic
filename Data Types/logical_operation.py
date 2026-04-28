0ee4df59484a26a2c7f92894a0f741ecf9183148
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-28 22:59:42 +0530

    Update logical_operation.py

--- Data Types/logical_operation.py
+++ Data Types/logical_operation.py
@@ -2,3 +2,11 @@ a = False and True    							# Output: False
 b = True or False      							# Output: True
 c = False and print("This won't print")  		# Output: False
 d = True or print("This won't print either")  	# Output: True
+
+a = 10
+b = 20
+c = 30
+
+print(a < b and b < c)  	# Output: True
+print(a < b or b > c)   	# Output: True
+print(not a > b)           	# Output: True
