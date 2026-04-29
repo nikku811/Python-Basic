974b58478d390595e74fbbcf60ce7818e3e71937
Ram Narayan <nikkuvskm45@gmail.com>
2026-04-29 23:40:08 +0530

    Create sum_of_natural_no.py

---
+++ Data Types/sum_of_natural_no.py
@@ -0,0 +1,8 @@
+# Input the value of n
+n = 10
+
+# Calculate the sum using the formula
+sum_n = n * (n + 1) // 2
+
+# Print the result
+print("The sum of the first", n, "natural numbers is:", sum_n)
