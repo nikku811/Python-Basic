54ce8ff37a14b6d67287b89d49bf2c2489a6baaf
Ram Narayan <nikkuvskm45@gmail.com>
2026-05-03 23:47:32 +0530

    Update if_else_2.py

---
+++ Data Types/if_else_2.py
@@ -0,0 +1,12 @@
+letter = "B"
+
+if letter == "A":
+    print("Letter is A")
+elif letter == "B":
+    print("Letter is B")
+elif letter == "C":
+    print("Letter is C")
+else:
+    print("Letter is not A, B, or C")
+    
+print("Program Ended")
